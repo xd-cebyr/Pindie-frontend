38cd9ad84004ff36cb93f0b2599bf93455cd2cbd
bestwizzar. <jiperschristmas@gmail.com>
2024-05-20 13:25:49 +0700

    ь

--- app/api/config.js
+++ app/api/config.js
@@ -2,6 +2,6 @@ export const BASE_URL = "http://localhost:3001/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/local`,
-  me: `${BASE_URL}/users/me`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
 };
